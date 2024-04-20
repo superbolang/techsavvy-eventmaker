748e408ff563da1f8e173814c42d392454c71616
boi <lorem4690@gmail.com>
2024-04-20 22:07:43 +0700

    Setup boilerplate

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,10 @@
 import Image from 'next/image';
 
 export default function Home() {
-  return <div>Under development</div>;
+  return (
+    <div>
+      <div>Under development</div>
+      <div>Task Setup Boilerplate</div>
+    </div>
+  );
 }
